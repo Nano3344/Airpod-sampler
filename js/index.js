052c362c29a93470f06128a4d8d5d69f16fd92d2
Malik Ebers <malik.ebers@web.de>
2020-01-14 17:04:56 +0100

    Added responsive nav bar

--- js/index.js
+++ js/index.js
@@ -33,3 +33,19 @@ function loadModal() {
 function closeModal() {
   document.getElementById("themodal").style.display = 'none';
 }
+
+// Navbar Animation
+
+var burgerNav = document.querySelector(".burger-nav");
+var fullNav = document.querySelector(".full-nav");
+var closeBtn = document.querySelector(".closeNav");
+burgerNav.addEventListener('click', loadNav);
+closeBtn.addEventListener('click', closeNav);
+
+function loadNav() {
+  document.getElementById("full-nav").style.display = 'block';
+}
+
+function closeNav() {
+  document.getElementById("full-nav").style.display = 'none';
+}
